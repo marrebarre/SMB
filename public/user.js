dc6664c012b02140abe289cd1b76c5ba9a37d195
Sebastian <sebastian.martensson95@gmail.com>
2021-03-11 17:05:22 +0100

    username and registration

--- public/user.js
+++ public/user.js
@@ -1,71 +1,74 @@
-function getStatus(){
+function getStatus() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
 
-        var data = this.responseText; 
-    }
+            var data = this.responseText;
+
+
+        }
     };
     xmlhttp.open("GET", "/status", true);
     xmlhttp.send();
 }
 //Check if there is session every sec
-setInterval(getStatus(),1000);
+setInterval(getStatus(), 1000);
 
-function getOverview(){
+function getOverview() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
 
-        var data = JSON.parse(this.responseText); 
+            var data = JSON.parse(this.responseText);
 
-        document.getElementById("data").innerHTML = "<br><table id='t'></table>";
-        document.getElementById("t").innerHTML = "";
-        for(var i= 0; i<data.length; i++){
-            document.getElementById("t").innerHTML += "<tr>"+
-            "<th>" +data[i].id+"</th>"+
-            "<th>" +data[i].name+"</th>"+
-            "<th>" + data[i].balance+"</th> </tr>"
+            document.getElementById("data").innerHTML = "<br><table id='t'></table>";
+            document.getElementById("t").innerHTML = "";
+            for (var i = 0; i < data.length; i++) {
+                document.getElementById("t").innerHTML += "<tr>" +
+                    "<th>" + data[i].id + "</th>" +
+                    "<th>" + data[i].name + "</th>" +
+                    "<th>" + data[i].balance + "</th> </tr>"
+                document.getElementById("displayUser").innerHTML = data.name;
+            }
+
         }
-                              
-    }
     };
     xmlhttp.open("GET", "/balance", true);
     xmlhttp.send();
 }
 
-function getTransfer(){
+function getTransfer() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200) {
 
-        var data = JSON.parse(this.responseText); 
+            var data = JSON.parse(this.responseText);
 
-        document.getElementById("data").innerHTML = "<br><select name='from' id='fromId'></select>";
-        for(var i= 0; i<data.length; i++){
-            document.getElementById("fromId").innerHTML += 
-            "<option value="+data[i].id+">"+data[i].name+"("+data[i].balance+")"+"</option> ";
-            
+            document.getElementById("data").innerHTML = "<br><select name='from' id='fromId'></select>";
+            for (var i = 0; i < data.length; i++) {
+                document.getElementById("fromId").innerHTML +=
+                    "<option value=" + data[i].id + ">" + data[i].name + "(" + data[i].balance + ")" + "</option> ";
+
+            }
+            document.getElementById("data").innerHTML += "<br>&#8595<br><select name='to' id='toId'></select>";
+            for (var i = 0; i < data.length; i++) {
+                document.getElementById("toId").innerHTML +=
+                    "<option value=" + data[i].id + ">" + data[i].name + "(" + data[i].balance + ")" + "</option> ";
+
+            }
+
+            const select = document.getElementById("fromId");
+
+            document.getElementById("data").innerHTML += "<br><input min='0' type='number' placeholder='SEK' name='amount' id='amountId' required><br><input type='submit' value='Transfer' onclick='transfer()'>";
+
+
         }
-        document.getElementById("data").innerHTML += "<br>&#8595<br><select name='to' id='toId'></select>";
-        for(var i= 0; i<data.length; i++){
-            document.getElementById("toId").innerHTML += 
-            "<option value="+data[i].id+">"+data[i].name+"("+data[i].balance+")"+"</option> ";
-            
-        }
-
-        const select = document.getElementById("fromId");
-
-        document.getElementById("data").innerHTML += "<br><input min='0' type='number' placeholder='SEK' name='amount' id='amountId' required><br><input type='submit' value='Transfer' onclick='transfer()'>";
-
-                     
-    }
     };
     xmlhttp.open("GET", "/balance", true);
     xmlhttp.send();
 }
 
-function transfer(){
+function transfer() {
     //XMLHTTP POST
 }
 
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var session = require('client-sessions');
 app.use(session({
     cookieName: 'session',
     secret: 'random_string_goes_here',
-    duration: 0.5 * 60 * 1000,
+    duration: 30 * 60 * 1000,
     activeDuration: 5 * 60 * 1000,
 
 }));
@@ -19,10 +19,10 @@ app.use(session({
 //DATABASE
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'root',
-database: 'bank'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bank'
 });
 
 
@@ -30,13 +30,13 @@ database: 'bank'
 //Home
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
-    
+
 });
 
 //Accounts
 app.get('/accounts', (req, res) => {
 
-    if(req.session && req.session.user){
+    if (req.session && req.session.user) {
         switch (req.session.user.admin) {
             case 1:
                 res.sendFile((__dirname + '/private/admin.html'));
@@ -44,84 +44,144 @@ app.get('/accounts', (req, res) => {
             case 0:
                 res.sendFile((__dirname + '/private/user.html'));
                 break;
-        
+
             default:
+                console.log("trying to log in from creation")
                 res.sendFile(__dirname + "/public/login.html");
                 break;
         }
-    }else{
+    } else {
+        console.log("testing login from account creating")
         res.sendFile(__dirname + "/public/login.html");
     }
 
-    
-    
+
+
 });
 
 //Log in
 app.get('/login', (req, res) => {
     res.sendFile(__dirname + "/public/login.html");
     req.session.reset();
-    
+
+});
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + "/public/register.html");
+    req.session.reset();
+
 });
 
 
 //DATABASE REQUESTS
-app.get('/balance',(req, res) => {
-    
-    connection.query("SELECT name, id, balance FROM account WHERE user_id='"+req.session.user.id+"'", function (err, rows, fields) {
+app.get('/balance', (req, res) => {
+
+    connection.query("SELECT name, id, balance FROM account WHERE user_id='" + req.session.user.id + "'", function(err, rows, fields) {
         if (err) throw err
 
-        
+
         res.json(rows);
     })
 });
 
-app.post('/validate',(req, res) => {
-    
-    var sql = "SELECT * FROM user WHERE username='"+req.body.username+"' AND password='"+req.body.password+"'";
+app.post('/validate', (req, res) => {
 
-    connection.query(sql, function (err, rows, fields) {
+    var sql = "SELECT * FROM user WHERE username='" + req.body.username + "' AND password='" + req.body.password + "'";
+
+    connection.query(sql, function(err, rows, fields) {
         if (err) throw err
 
-        if(rows.length == 0){
+        if (rows.length == 0) {
             console.log("No user found.")
-        }else{
+        } else {
             req.session.user = rows[0];
             res.redirect('/accounts');
         }
-        
+
     })
 });
 
-app.post('/validate',(req, res) => {
-    
-    var sql = "SELECT * FROM user WHERE username='";
+app.get('/status', (req, res) => {
 
-    connection.query(sql, function (err, rows, fields) {
-        if (err) throw err
-
-        if(rows.length == 0){
-            res.send("No user found.")
-        }else{
-            
-            res.json(rows);
-        }
-        
-    })
-});
-
-app.get('/status',(req, res) => {
-    
-    if(req.session && req.session.user){
+    if (req.session && req.session.user) {
         res.json(req.session.user.username);
-    }else{
+    } else {
         res.redirect('/login');
     }
-    
+
+});
+
+app.post('/createNew', (req, res) => {
+    var checking = "1234567890 [`!@#$%^&*()_+-=[]{};':|,.<>/?~]^[0-9]+$/";
+    var bool = false;
+
+    const uname = req.body.username;
+
+    for (var i = 0; i < checking.length; i++) {
+        if (uname.includes(checking.charAt(i))) {
+            bool = true;
+        }
+    }
+
+
+    if (bool) {
+        console.log("username contains errors");
+    } else {
+        var sqlCheckAccount = "SELECT * FROM user WHERE username='" + req.body.username + "'";
+        var sqlCreateAccount = "INSERT INTO user (username, password, admin) VALUES ('" + req.body.username + "', '" + req.body.password + "', '0')";
+
+        var sqlGetUser = "SELECT * FROM user WHERE username='" + req.body.username + "' AND password='" + req.body.password + "'";
+        connection.query(sqlCheckAccount, function(err, rows, fields) {
+            if (err) throw err
+            if (rows.length == 0) {
+                //we can create account
+                console.log("we can create account");
+                connection.query(sqlCreateAccount);
+                connection.query(sqlGetUser, function(err, rows, fields) {
+                    req.session.user = rows[0];
+                    res.redirect('/accounts');
+                });
+            } else {
+                console.log("Error, couldnt create a ");
+
+            }
+        })
+    }
+
+
+
+    /*if (nan(req.body.username) || format.test(req.body.username)) {
+        console.log("username contains errors");
+    } else {
+        var sqlCheckAccount = "SELECT * FROM user WHERE username='" + req.body.username + "'";
+        var sqlCreateAccount = "INSERT INTO user (username, password, admin) VALUES ('" + req.body.username + "', '" + req.body.password + "', '0')";
+
+        var sqlGetUser = "SELECT * FROM user WHERE username='" + req.body.username + "' AND password='" + req.body.password + "'";
+        connection.query(sqlCheckAccount, function(err, rows, fields) {
+            if (err) throw err
+            if (rows.length == 0) {
+                //we can create account
+                console.log("we can create account");
+                connection.query(sqlCreateAccount);
+                connection.query(sqlGetUser, function(err, rows, fields) {
+                    req.session.user = rows[0];
+                    res.redirect('/accounts');
+                });
+            } else {
+                console.log("Error, couldnt create a ");
+
+            }
+        })
+    }*/
 });
 
 
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function nan(num) {
+    return !isNaN(num);
+}
 
 
-
-app.listen(80, () => console.log('Server running on port 80!'));
+app.listen(8080, () => console.log('Server running on port 8080!'));
